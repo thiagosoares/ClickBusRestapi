4b79caebc4c3bae0f4da94442f9b195bc35a11ae
Thiago Soares <thiago.soares.jr@gmail.com>
2018-11-01 18:01:29 -0300

    Adicao das implemantaces padrao para os servicos

---
+++ src/main/java/com/clickbus/service/service/impl/CityServiceImpl.java
@@ -0,0 +1,114 @@
+package com.clickbus.service.service.impl;
+
+import com.clickbus.service.service.CityService;
+import com.clickbus.service.domain.City;
+import com.clickbus.service.repository.CityRepository;
+import com.clickbus.service.repository.search.CitySearchRepository;
+import com.clickbus.service.service.dto.CityDTO;
+import com.clickbus.service.service.mapper.CityMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+import static org.elasticsearch.index.query.QueryBuilders.*;
+
+/**
+ * Service Implementation for managing City.
+ */
+@Service
+@Transactional
+public class CityServiceImpl implements CityService {
+
+    private final Logger log = LoggerFactory.getLogger(CityServiceImpl.class);
+
+    private CityRepository cityRepository;
+
+    private CityMapper cityMapper;
+
+    private CitySearchRepository citySearchRepository;
+
+    public CityServiceImpl(CityRepository cityRepository, CityMapper cityMapper, CitySearchRepository citySearchRepository) {
+        this.cityRepository = cityRepository;
+        this.cityMapper = cityMapper;
+        this.citySearchRepository = citySearchRepository;
+    }
+
+    /**
+     * Save a city.
+     *
+     * @param cityDTO the entity to save
+     * @return the persisted entity
+     */
+    @Override
+    public CityDTO save(CityDTO cityDTO) {
+        log.debug("Request to save City : {}", cityDTO);
+
+        City city = cityMapper.toEntity(cityDTO);
+        city = cityRepository.save(city);
+        CityDTO result = cityMapper.toDto(city);
+        citySearchRepository.save(city);
+        return result;
+    }
+
+    /**
+     * Get all the cities.
+     *
+     * @param pageable the pagination information
+     * @return the list of entities
+     */
+    @Override
+    @Transactional(readOnly = true)
+    public Page<CityDTO> findAll(Pageable pageable) {
+        log.debug("Request to get all Cities");
+        return cityRepository.findAll(pageable)
+            .map(cityMapper::toDto);
+    }
+
+
+    /**
+     * Get one city by id.
+     *
+     * @param id the id of the entity
+     * @return the entity
+     */
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<CityDTO> findOne(Long id) {
+        log.debug("Request to get City : {}", id);
+        return cityRepository.findById(id)
+            .map(cityMapper::toDto);
+    }
+
+    /**
+     * Delete the city by id.
+     *
+     * @param id the id of the entity
+     */
+    @Override
+    public void delete(Long id) {
+        log.debug("Request to delete City : {}", id);
+        cityRepository.deleteById(id);
+        citySearchRepository.deleteById(id);
+    }
+
+    /**
+     * Search for the city corresponding to the query.
+     *
+     * @param query the query of the search
+     * @param pageable the pagination information
+     * @return the list of entities
+     */
+    @Override
+    @Transactional(readOnly = true)
+    public Page<CityDTO> search(String query, Pageable pageable) {
+        log.debug("Request to search for a page of Cities for query {}", query);
+        return citySearchRepository.search(queryStringQuery(query), pageable)
+            .map(cityMapper::toDto);
+    }
+}
---
+++ src/main/java/com/clickbus/service/service/impl/PlaceServiceImpl.java
@@ -0,0 +1,123 @@
+package com.clickbus.service.service.impl;
+
+import com.clickbus.service.service.PlaceService;
+import com.clickbus.service.domain.Place;
+import com.clickbus.service.repository.PlaceRepository;
+import com.clickbus.service.repository.search.PlaceSearchRepository;
+import com.clickbus.service.service.dto.PlaceDTO;
+import com.clickbus.service.service.mapper.PlaceMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+import static org.elasticsearch.index.query.QueryBuilders.*;
+
+/**
+ * Service Implementation for managing Place.
+ */
+@Service
+@Transactional
+public class PlaceServiceImpl implements PlaceService {
+
+    private final Logger log = LoggerFactory.getLogger(PlaceServiceImpl.class);
+
+    private PlaceRepository placeRepository;
+
+    private PlaceMapper placeMapper;
+
+    private PlaceSearchRepository placeSearchRepository;
+
+    public PlaceServiceImpl(PlaceRepository placeRepository, PlaceMapper placeMapper, PlaceSearchRepository placeSearchRepository) {
+        this.placeRepository = placeRepository;
+        this.placeMapper = placeMapper;
+        this.placeSearchRepository = placeSearchRepository;
+    }
+
+    /**
+     * Save a place.
+     *
+     * @param placeDTO the entity to save
+     * @return the persisted entity
+     */
+    @Override
+    public PlaceDTO save(PlaceDTO placeDTO) {
+        log.debug("Request to save Place : {}", placeDTO);
+
+        Place place = placeMapper.toEntity(placeDTO);
+        place = placeRepository.save(place);
+        PlaceDTO result = placeMapper.toDto(place);
+        placeSearchRepository.save(place);
+        return result;
+    }
+
+    /**
+     * Get all the places.
+     *
+     * @param pageable the pagination information
+     * @return the list of entities
+     */
+    @Override
+    @Transactional(readOnly = true)
+    public Page<PlaceDTO> findAll(Pageable pageable) {
+        log.debug("Request to get all Places");
+        return placeRepository.findAll(pageable)
+            .map(placeMapper::toDto);
+    }
+
+    /**
+     * Get all the Place with eager load of many-to-many relationships.
+     *
+     * @return the list of entities
+     */
+    public Page<PlaceDTO> findAllWithEagerRelationships(Pageable pageable) {
+        return placeRepository.findAllWithEagerRelationships(pageable).map(placeMapper::toDto);
+    }
+    
+
+    /**
+     * Get one place by id.
+     *
+     * @param id the id of the entity
+     * @return the entity
+     */
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<PlaceDTO> findOne(Long id) {
+        log.debug("Request to get Place : {}", id);
+        return placeRepository.findOneWithEagerRelationships(id)
+            .map(placeMapper::toDto);
+    }
+
+    /**
+     * Delete the place by id.
+     *
+     * @param id the id of the entity
+     */
+    @Override
+    public void delete(Long id) {
+        log.debug("Request to delete Place : {}", id);
+        placeRepository.deleteById(id);
+        placeSearchRepository.deleteById(id);
+    }
+
+    /**
+     * Search for the place corresponding to the query.
+     *
+     * @param query the query of the search
+     * @param pageable the pagination information
+     * @return the list of entities
+     */
+    @Override
+    @Transactional(readOnly = true)
+    public Page<PlaceDTO> search(String query, Pageable pageable) {
+        log.debug("Request to search for a page of Places for query {}", query);
+        return placeSearchRepository.search(queryStringQuery(query), pageable)
+            .map(placeMapper::toDto);
+    }
+}
