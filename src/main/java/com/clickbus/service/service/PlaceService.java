847d1c1a0869c0f2a88998fd9c1401e505494e64
Thiago Soares <thiago.soares.jr@gmail.com>
2018-11-01 17:51:45 -0300

    Adicao das interfaces para os services (CRUD)

---
+++ src/main/java/com/clickbus/service/service/PlaceService.java
@@ -0,0 +1,62 @@
+package com.clickbus.service.service;
+
+import com.clickbus.service.service.dto.PlaceDTO;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+/**
+ * Service Interface for managing Place.
+ */
+public interface PlaceService {
+
+    /**
+     * Save a place.
+     *
+     * @param placeDTO the entity to save
+     * @return the persisted entity
+     */
+    PlaceDTO save(PlaceDTO placeDTO);
+
+    /**
+     * Get all the places.
+     *
+     * @param pageable the pagination information
+     * @return the list of entities
+     */
+    Page<PlaceDTO> findAll(Pageable pageable);
+
+    /**
+     * Get all the Place with eager load of many-to-many relationships.
+     *
+     * @return the list of entities
+     */
+    Page<PlaceDTO> findAllWithEagerRelationships(Pageable pageable);
+    
+    /**
+     * Get the "id" place.
+     *
+     * @param id the id of the entity
+     * @return the entity
+     */
+    Optional<PlaceDTO> findOne(Long id);
+
+    /**
+     * Delete the "id" place.
+     *
+     * @param id the id of the entity
+     */
+    void delete(Long id);
+
+    /**
+     * Search for the place corresponding to the query.
+     *
+     * @param query the query of the search
+     * 
+     * @param pageable the pagination information
+     * @return the list of entities
+     */
+    Page<PlaceDTO> search(String query, Pageable pageable);
+}
